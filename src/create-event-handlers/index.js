6157773fd2edc7b36bd84622e30d69c93a935dc6
Gregory Shilin <gshilin@gmail.com>
2017-06-03 10:21:36 +0300

    Update index.js

--- src/create-event-handlers/index.js
+++ src/create-event-handlers/index.js
@@ -2,6 +2,7 @@ import onAdPlay from './on-ad-play';
 import onBeforePlay from './on-before-play';
 import onFullScreen from './on-full-screen';
 import onMute from './on-mute';
+import onMeta from './on-meta';
 import onPlay from './on-play';
 import onTime from './on-time';
 import onVideoLoad from './on-video-load';
@@ -12,6 +13,7 @@ function createEventHandlers(component) {
     onBeforePlay: onBeforePlay.bind(component),
     onFullScreen: onFullScreen.bind(component),
     onMute: onMute.bind(component),
+    onMeta: onMeta.bind(component),
     onPlay: onPlay.bind(component),
     onTime: onTime.bind(component),
     onVideoLoad: onVideoLoad.bind(component),
